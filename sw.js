233b0c498fc25c9d3a22b258fd2ba658ffa8c500
youssefnassar314-sudo <youssefnassar314@gmail.com>
2026-04-01 14:52:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
 
-self.addEventListener('fetch', (e) => {
-    // Basic fetch event para pumasa sa PWA requirements
-});
